d774e498c63c4a71fb9e19c00a283ee179b4f58d
arsh <bishtlakshay12@gmail.com>
2025-10-15 14:30:26 +0530

    Initialized

--- src/main/java/com/security/base/BaseController.java
+++ src/main/java/com/security/base/BaseController.java
@@ -10,7 +10,7 @@ public class BaseController {
 
     @GetMapping
     public String getHello() {
-        return "Hello From Main - Prod Demo";
+        return "Hello From Main - Prod Demo demo to ajy";
     }
 
 }
